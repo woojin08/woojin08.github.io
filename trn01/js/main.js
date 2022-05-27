25dcd3accbca3a53a15feae274657b961077e183
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-05-27 18:27:56 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,11 +1,20 @@
 $(function () {
 
+    $('.main_slider').on('init reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on')
+
+    })
+
     $('.main_slider').slick({
-        arrows:false,
-        autoplay:true,
+        arrows: false,
+        autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
-    })
+        fade: true,
+    });
+
+
 
 
 
