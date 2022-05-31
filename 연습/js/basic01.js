1cadf27de62446ddc5da25e29b272c3f349a05cf
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-05-31 18:18:01 +0900

    Add files via upload

---
+++ 연습/js/basic01.js
@@ -0,0 +1,23 @@
+document.querySelector('.mopen').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('on');
+    this.classList.toggle('on');
+})
+
+$(function () {
+
+    $('.main_slider').on('init reInit afterChange', function (e, s, c) {
+        console.log(e, s, c);
+        $('.num').text(c + 1 + "/" + s.slideCount)
+        let cur = $('.slick-current');
+        cur.addClass('xx').siblings().removeClass('xx');
+    });
+
+    $('.main_slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+})
