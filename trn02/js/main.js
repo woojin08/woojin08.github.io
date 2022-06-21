b2a522e6bfb8d96ac876b20b5cd0dc6bce752241
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-06-21 09:24:53 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+    // 20220620 jin topBanner
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').slideUp();
+    });
+    $('.mainSlider').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('mainArrow i').removeClass('on');
+        if (c === 1) {
+            $('mainArrow i').addClass('on')
+        }
+
+        // if (c) {
+        //     c = c + 1;
+
+        // } else {
+        //     c = 1
+        // }
+        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+    });
+
+    $('.mainSlider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        pauseOnHover: false,
+        pauseFocus: false,
+    });
+    $('.mainArrow i:nth-child(1)').on('click', function () {
+        $('.mainSlider').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function () {
+        $('.mainSlider').slick('slickNext')
+    });
+})
