21a76ae8b84b7a34a4601bc88f218bd7359c9710
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-06-23 15:41:19 +0900

    Add files via upload

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -33,4 +33,73 @@ $(function () {
     $('.mainArrow i:nth-child(2)').on('click', function () {
         $('.mainSlider').slick('slickNext')
     });
+
+    $('#ytb').YTPlayer({
+        videoURL: 'https://youtu.be/dS9x5Ws67wI',
+        containment: '.ytb',
+        autoPlay: true,
+        mute: true,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+    $('.ytb .zoom').on('click', function (e) {
+        e.preventDefault();
+        $('#ytb').YTPFullscreen()
+    });
+
+    $('.productSlider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 5,
+        centerMode: true,
+    });
+
+
+    $('.product .productArrows i:nth-child(1)').on('click', function () {
+        $('.productSlider').slick('slickPrev')
+    });
+    $('.product .productArrows i:nth-child(2)').on('click', function () {
+        $('.productSlider').slick('slickNext')
+    });
+
+
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        //console.log($(this), $(this).index(), event.target, event.currentTarget);
+        $('.tab_menu li').removeClass('on');
+        $(this).addClass('on');
+        var idx = $(this).index(); // 0,1,2
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
+
+
+    });
+
+    $('.tab_content .notice li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index(); // 0,1,2
+        $('.right_content>li').removeClass('on');
+        $('.right_content>li').eq(idx).addClass('on');
+        console.log(ux)
+    });
+
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300)
+        } else {
+            $('.toTop').fadeOut(1000)
+        }
+    })
+
+
+
 })
