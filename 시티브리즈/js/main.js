e3013cce786098d83f47db7b052803d2b3a0eae2
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-07-12 17:43:46 +0900

    Add files via upload

---
+++ 시티브리즈/js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+    // 20220620 jin topBanner
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').slideUp();
+    });
+    $('.mainSlider').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('mainArrow i').removeClass('on');
+        if (c === 1) {
+            $('mainArrow i').addClass('on')
+        }
+
+        // if (c) {
+        //     c = c + 1;
+
+        // } else {
+        //     c = 1
+        // }
+        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+    });
+
+    $('.mainSlider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        pauseOnHover: false,
+        pauseFocus: false,
+    });
+
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        //console.log($(this), $(this).index(), event.target, event.currentTarget);
+        $('.tab_menu li').removeClass('on');
+        $(this).addClass('on');
+        var idx = $(this).index(); // 0,1,2
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
+
+
+    });
+    $('.tab_content .notice li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index(); // 0,1,2
+        $('.right_content>li').removeClass('on');
+        $('.right_content>li').eq(idx).addClass('on');
+        console.log(ux)
+    });
+
+})
+
