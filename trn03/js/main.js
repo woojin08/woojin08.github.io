edf99229ce16514e468f25d0ef1e41c08ec99176
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-07-28 17:34:56 +0900

    Add files via upload

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -1,9 +1,3 @@
 $(function () {
-    $('.TopBanner i').on('click', function () {
-        $('.TopBanner').slideUp();
-    });
 
-    $('.pop button').on('click', function () {
-        $(this).parent().hide();
-    })
 })
