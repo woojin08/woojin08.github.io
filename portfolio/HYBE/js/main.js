eaf9d062041d54d6234beddd6264c17c82c8d962
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-08-04 17:46:33 +0900

    Add files via upload

--- portfolio/HYBE/js/main.js
+++ portfolio/HYBE/js/main.js
@@ -1,192 +1,167 @@
 $(function () {
-    // 20220620 jin topBanner
-    $('.topBanner i').on('click', function () {
-        $('.topBanner').slideUp(1);
-    });
-    $('.mainSlider').on('init afterChange', function (e, s, c) {
-        var current = $('.slick-current');
-        current.addClass('on').siblings().removeClass('on');
-        $('mainArrow i').removeClass('on');
-        if (c === 1) {
-            $('mainArrow i').addClass('on')
-        }
-
-        // if (c) {
-        //     c = c + 1;
-
-        // } else {
-        //     c = 1
-        // }
-        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+    $('header i').on('click', function () {
+        $('.cover').toggleClass('on')
     });
 
-    $('.mainSlider').slick({
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        pauseOnHover: false,
-        pauseFocus: false,
-        fade: true,
-    });
-
-    $('.mainSlider').on('init afterChange', function (e, s, c) {
-        console.log(e, s, c);
-        $('.mainVisual .box .num').text("0" + (c ? c + 1 : 1) + " / " + "0" + s.slideCount)
-        $('.mainVisual .content>li').eq(c).addClass('on').siblings().removeClass('on')
+    $('.cover a').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).parent().index();
+        console.log(100 * idx + "%", $('.sticky').height());
+        let h = $('.sticky').height();
+        $('.cover').removeClass('on');
+        $('html, body').animate({ scrollTop: idx * (h + 1) }, 1000)
     })
-
-    // $('.btn_years a').on('click', function (event) {
-    //     event.preventDefault();
-    //     //console.log($(this), $(this).index(), event.target, event.currentTarget);
-    //     $('.btn_years a').removeClass('on');
-    //     $(this).addClass('on');
-    //     var idx = $(this).index(); // 0,1,2
-    //     $('.tab_content>div').removeClass('on');
-    //     $('.tab_content>div').eq(idx).addClass('on');
-
-
-    // });
-    // $('.tab_content .notice li').on('click', function (event) {
-    //     event.preventDefault();
-    //     var idx = $(this).index(); // 0,1,2
-    //     $('.right_content>li').removeClass('on');
-    //     $('.right_content>li').eq(idx).addClass('on');
-    //     console.log(ux)
-    // });
-    // $(".event-slider").slick({
-    //     arrows: false,
-    //     //autoplay:true,
-    //     autoplaySpeed: 3000,
-    //     pauseOnHover: false,
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    // });
-
-    var contentSlider01 = $(".content-slider").slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 400,
-        //centerMode: true,
-        //variableWidth: true,
-        //https://cubic-bezier.com/ 베지어 곡선 만들기
-        //cssEase:"cubic-bezier(.69,-1.31,.74,1.17)",
-        cssEase: "cubic-bezier(0,0,.58,1)",
-    });
-    var contentSlider02 = $(".content-slider01").slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        dots: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 400,
-        //centerMode: true,
-        //variableWidth: true,
-        //https://cubic-bezier.com/ 베지어 곡선 만들기
-        //cssEase:"cubic-bezier(.69,-1.31,.74,1.17)",
-        cssEase: "cubic-bezier(0,0,.58,1)",
-    });
-
-    var contentPrev = $("#content01 i.xi-angle-left-thin");
-    var contentNext = $("#content01 i.xi-angle-right-thin");
-
-    contentPrev.on("click", function () {
-        contentSlider01.slick("slickPrev")
-    });
-    contentNext.on("click", function () {
-        contentSlider01.slick("slickNext")
-    });
-
-    var contentItem = $(".content-slider figure");
-    contentItem.eq(3).addClass("on")
-    contentSlider01.on("afterChange", function (event, slick, current) {
-        contentItem.eq(current + 3).addClass("on").siblings().removeClass("on");
-    });
-
-
 })
 
-$(".event-slider").slick({
-    arrows: false,
-    //autoplay:true,
-    autoplaySpeed: 2000,
-    pauseOnHover: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+
+// song 슬라이드
+
+var songSlider = $('.song-contents .slider-wrap'),
+    songItem = songSlider.find('.slide'),
+    songBtnPrev = songSlider.find('.btn.prev'),
+    songBtnPrevFirst = songSlider.find('.btn.prev.first'),
+    songBtnNext = songSlider.find('.btn.next'),
+    songPgAll = songSlider.find('.all'),
+    songProcessBar = songSlider.find('.process');
+if (songItem.length < 10) {
+    songPgAll.html("0" + songItem.length);
+} else {
+    songPgAll.html(songItem.length);
+}
+
+songItem.each(function (index, count) {
+    songItem.eq(index).css('z-index', '-' + index).attr('data', index + 1);
+});
+function songNum() {
+    var num = songSlider.find('.on').attr('data');
+    var songPgNow = songSlider.find('.now');
+    // songPgNow.html("0"+num);
+
+    num = Number(num).toString();
+    if (Number(num) < 10 && num.length == 1)
+        num = "0" + num;
+    songPgNow.html(num);
+    // return num;
+}
+
+function songProcess() {
+    // const target = songSlider.find('.process');
+    songProcessBar.removeClass("on").removeClass('pause'),
+        void songProcessBar.outerWidth(),
+        songProcessBar.addClass("on");
+}
+
+$('body').on('click', '.first', function (e) {
+    songLast();
 });
 
-$(".event-slider").eq(0).slick("slickPlay");
+function songFirst() {
 
-//슬라이드 탭
-$("#event li").on("click", function () {
-    let idx = $(this).index();
-    //console.log(idx);
-    $(this).addClass("on").siblings().removeClass("on");
-    $(".event-slider").eq(idx).addClass("on").siblings().removeClass("on");
-    $(".event-slider").eq(idx).slick("slickPlay").siblings().slick("slickPause")
-});
+    songBtnPrev.addClass('first');
+    songProcess();
+    // var item = $('.slide');
+    songItem.each(function (index, count) {
+        songPrev(true);
+    });
+}
 
-$(function () {
-    var cnt0 = 0;
-
-    $(window).on('scroll', function () {
-        var sct = $(window).scrollTop();
-        if (sct > $(".number").offset().top - 400) {
-            counterFn();
-        }
-
-    })
-
-
-
-    function counterFn() {
-
-        id0 = setInterval(count0Fn, 1);
-
-        function count0Fn() {
-            cnt0++;
-            if (cnt0 > 709) {
-                clearInterval(id0);
-            } else {
-                $(".number").text(cnt0);
-            }
-        }
+function songLast() {
+    // songItem.each(function (index, count) {
+    //     for(var i = 1;i<index;i++){
+    //         songNext(true);
+    //     }
+    // });
+    var count = songItem.length - 1;
+    for (var i = 0; i < count; i++) {
+        songNext(true);
     }
+}
+let playing = true;
+let lastNextCall = new Date();
+
+function songPrev(force) {
+    if (!playing) return;
+    if ((force || !lastNextCall || new Date() - lastNextCall > 3000)) {
+        var second = $("li.on").attr('data');
+        // console.log(second)
+        if (second == '1') {
+            songBtnPrev.addClass('first');
+        }
+        songProcess();
+        songSlider.find('.on').addClass('next').siblings().removeClass('next');
+        songSlider.find('.on').prev().addClass('on').removeClass('prev').siblings().removeClass('on');
+    }
+}
+
+function songNext(force) {
+
+    songNum();
+    if (!playing) return;
+    if ((force || !lastNextCall || new Date() - lastNextCall > 3000)) {
+        songBtnPrev.removeClass('first');
+        songProcess();
+        lastNextCall = new Date();
+        var songLast = songSlider.find('.slide.on').next().length;
+        if (songLast == '0') {
+            songFirst();
+            return;
+        }
+        songSlider.find('.on').addClass('prev').siblings().removeClass('next');
+        songSlider.find('.on').next().addClass('on').siblings().removeClass('on');
+    }
+}
+setInterval(songNext, 100);
+songBtnNext.on('click', function () {
+    songNext(true);
+    songNum();
+});
+songBtnPrev.on('click', function () {
+    songPrev(true);
+    songNum();
 });
 
-$('.pr_slider figure').eq(4).addClass('on');
-$('.pr_slider').on('afterChange', function (e, s, c) {
-    $('.pr_slider figure').eq(c + 4).addClass('on').siblings().removeClass('on');
+songSlider.find('> ul').on('mouseover', function (e) {
+    playing = false;
+    lastNextCall = new Date();
+    songProcessBar.addClass('pause');
+});
+songSlider.find('> ul').on('mouseout', function (e) {
+    playing = true;
+    lastNextCall = new Date();
+    songProcess();
 });
 
-$('.pr_slider').slick({
+//content-slider
+var contentSlider01 = $(".content-slider").slick({
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
-    centerPadding: '5rem',
-    slidesToShow: 3,
+    autoplaySpeed: 5000,
     pauseOnHover: false,
     pauseOnFocus: false,
-    dots: false,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 400,
+    //centerMode: true,
+    //variableWidth: true,
+    //https://cubic-bezier.com/ 베지어 곡선 만들기
+    //cssEase:"cubic-bezier(.69,-1.31,.74,1.17)",
+    cssEase: "cubic-bezier(0,0,.58,1)",
 });
 
-// popup
-document.querySelector('.popup_one button').addEventListener('click', function () {
-    console.log('바부팅');
-    document.querySelector('.popup_one').style.display = 'none';
-})
-document.querySelector('.popup_two button').addEventListener('click', function () {
-    console.log('바부팅');
-    document.querySelector('.popup_two').style.display = 'none';
-})
+var contentPrev = $("artist-wrap i.xi-angle-left");
+var contentNext = $("artist-wrap i.xi-angle-right");
+
+contentPrev.on("click", function () {
+    artist - wrap.slick("slickPrev")
+});
+contentNext.on("click", function () {
+    artist - wrap.slick("slickNext")
+});
+
+var contentItem = $(".content-slider figure");
+contentItem.eq(3).addClass("on")
+contentSlider01.on("afterChange", function (event, slick, current) {
+    contentItem.eq(current + 3).addClass("on").siblings().removeClass("on");
+});
+
