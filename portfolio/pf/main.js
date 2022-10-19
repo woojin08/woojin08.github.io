160c47690f1071ade3c348f0396863b75a551aaa
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-10-19 17:48:59 +0900

    Add files via upload

--- portfolio/pf/main.js
+++ portfolio/pf/main.js
@@ -20,8 +20,6 @@ $(function () {
     });
 
 
-
-
     $('.basicSlider').slick({
         arrows: false,
         autoplay: true,
@@ -51,3 +49,12 @@ $(function () {
 
 })
 
+// sss
+var docStyle = document.documentElement.style;
+
+// setting CSS variables inside this JS where they 
+// can be declaratively used inside the CSS file.
+document.addEventListener('mousemove', function (e) {
+    docStyle.setProperty('--mouse-x', e.clientX);
+    docStyle.setProperty('--mouse-y', e.clientY);
+});
