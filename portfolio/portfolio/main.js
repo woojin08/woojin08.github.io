d7b91e9619ea676018c7b513559c0a468d51a71a
woojin08 <105770875+woojin08@users.noreply.github.com>
2022-11-03 19:46:48 +0900

    Add files via upload

--- portfolio/portfolio/main.js
+++ portfolio/portfolio/main.js
@@ -49,85 +49,48 @@ $(function () {
         event.preventDefault()
     })
 
-    // main effect
+    // bubble
 
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('resize', handleWindowResize);
+    jQuery(document).ready(function ($) {
 
-    const spansSlow = document.querySelectorAll('.spanSlow');
-    const spansFast = document.querySelectorAll('.spanFast');
+        //effect positions.
+        var bArray = [];
+        //bubble sizes
+        var sArray = [4, 6, 8, 10];
 
-    let width = window.innerWidth;
+        // Push the header width values to bArray
+        for (var i = 0; i < $('.bubbles').width(); i++) {
+            bArray.push(i);
+        }
 
-    function handleMouseMove(e) {
-        let normalizedPosition = e.pageX / (width / 2) - 1;
-        let speedSlow = 100 * normalizedPosition;
-        let speedFast = 200 * normalizedPosition;
-        spansSlow.forEach((span) => {
-            span.style.transform = `translate(${speedSlow}px)`;
-        });
-        spansFast.forEach((span) => {
-            span.style.transform = `translate(${speedFast}px)`
-        })
-    }
-    //we need to recalculate width when the window is resized
-    function handleWindowResize() {
-        width = window.innerWidth;
-    }
+        //시간
+        function randomValue(arr) {
+            return arr[Math.floor(Math.random() * arr.length)];
+        }
 
+        // 
+        setInterval(function () {
+
+            // Get a random size, width and height
+            var size = randomValue(sArray);
+            // New bubble  size and left position 
+            $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+
+            //animations from the page
+            $('.individual-bubble').animate({
+                'bottom': '100%',
+                'opacity': '-=0.7'
+            }, 3000, function () {
+                $(this).remove()
+            }
+            );
+
+
+        }, 350);
+
+    });
 
 })
 
-$(document).ready(function () {
-
-    //    pf 슬라이드 쇼
-    $('.pf-right').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        prevArrow: false,
-        nextArrow: false,
-        autoplay: true,
-        autoplaySpeed: 8600,
-    });
-    //      pf 슬라이드 애니메이션
-    $(".slick-slide").removeClass("on");
-    $(".slick-current").addClass("on");
-
-    //    디자인 팝업 스크롤
 
 
-});
-
-
-// The place I saw this effect:
-// https://angle2.agency/
-
-// Red Staper made a NICE tutorial about this effect! You should check it out!
-// https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
-
-window.addEventListener('mousemove', handleMouseMove);
-window.addEventListener('resize', handleWindowResize);
-
-const spansSlow = document.querySelectorAll('.spanSlow');
-const spansFast = document.querySelectorAll('.spanFast');
-
-let width = window.innerWidth;
-
-function handleMouseMove(e) {
-    let normalizedPosition = e.pageX / (width / 2) - 1;
-    let speedSlow = 100 * normalizedPosition;
-    let speedFast = 200 * normalizedPosition;
-    spansSlow.forEach((span) => {
-        span.style.transform = `translate(${speedSlow}px)`;
-    });
-    spansFast.forEach((span) => {
-        span.style.transform = `translate(${speedFast}px)`
-    })
-}
-//we need to recalculate width when the window is resized
-function handleWindowResize() {
-    width = window.innerWidth;
-}
