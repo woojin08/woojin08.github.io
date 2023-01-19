1abaf8cc061e63143252462b66e4e3d2e639ca73
woojin <105770875+woojin08@users.noreply.github.com>
2023-01-19 17:56:00 +0900

    Add files via upload

--- movie_ost/js/main.js
+++ movie_ost/js/main.js
@@ -19,4 +19,22 @@ var swiper = new Swiper(".mySwiper", {
     crossFade: true,
 },
   });
+var swiper = new Swiper(".sitllSwiper", {
+    spaceBetween: 30, //슬라이드 사이 여백
+    centeredSlides: true, //센터모드
+   
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop :true,   // 슬라이드 반복 여부
+    navigation: { //버튼
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+//     effect : 'fade', 
+//     fadeEffect: { 
+//     crossFade: true,
+// },
+  });
   
