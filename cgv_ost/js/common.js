05e08ef6b009eda7627fa7557d6264ee2e4dad03
woojin <105770875+woojin08@users.noreply.github.com>
2023-02-15 17:44:04 +0900

    Add files via upload

--- cgv_ost/js/common.js
+++ cgv_ost/js/common.js
@@ -29,3 +29,46 @@ var swiper = new Swiper(".mySwiper", {
 },
 });
 
+// totop
+
+//11111
+$(document).ready(function() {
+  console.log($(this).scrollTop());
+  if ($(this).scrollTop() == 0) {
+      $(".iconbutton").hide();
+  }
+
+  $(document).scroll(function() {
+      if ($(this).scrollTop() > 0) {
+          $(".iconbutton").show();
+      } else {
+          $(".iconbutton").hide();
+      }
+  })
+
+  $(".iconbutton").click(function() {
+      //바디 스크롤 제일 상위로 이동
+      $("html,body").animate({
+          scrollTop: 0
+      }, 700)
+  })
+
+})
+
+
+
+// 2222
+// $(window).scroll(function () {
+//   var quickHeight=$(document).scrollTop(); //스크롤 높이가 500 이상이면 나타나기
+//   if (500 <= quickHeight ) {
+//     $('.ac-sub-go-top').css('display','block');
+//    }else {
+//      $('.ac-sub-go-top').css('display','none');
+//    }
+//  });
+
+//  $('.ac-sub-go-top').click(function(){//위로가기 버튼을 클릭했을때
+//    $('html, body').animate({
+//      scrollTop: '0'
+//    }, 800);
+//  });
